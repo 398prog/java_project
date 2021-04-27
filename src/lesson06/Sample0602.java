11712a4c7819cb8c31ef31d43c20396e0a091b51
398 <51398201+398prog@users.noreply.github.com>
2021-04-27 17:55:41 +0900

    第6章　サンプルコード
    予習で書いたサンプルコード
    演算子がメイン

---
+++ src/lesson06/Sample0602.java
@@ -0,0 +1,17 @@
+package lesson06;
+
+public class Sample0602 {
+
+	public static void main(String[] args) {
+		int num1 = 3;
+		int num2 = 5;
+
+		int sum = num1 + num2;
+
+		System.out.println("変数num1の値は"+num1+"です。");
+		System.out.println("変数num2の値は"+num2+"です。");
+		System.out.println("num1+num2の値は"+sum+"です。");
+
+	}
+
+}
---
+++ src/lesson06/Sample0603.java
@@ -0,0 +1,15 @@
+package lesson06;
+
+public class Sample0603 {
+
+	public static void main(String[] args) {
+		int num1 = 1;
+		int num2 = 0;
+
+//		num2 = num1++;
+		num2 = ++num1;
+		System.out.println("変数num2の値は"+num2+"です。");
+
+	}
+
+}
